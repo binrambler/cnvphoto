6d48b4fa2a01fa43b2878b6fdde6a87512b5a8c6
binrambler <104247888+binrambler@users.noreply.github.com>
2022-06-15 16:23:14 +0400

    Add files via upload

--- cnvphoto.py
+++ cnvphoto.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from configparser import ConfigParser
 from PIL import Image
+import sys
 
 CURR_DIR = Path.cwd()
 FILE_INI = Path(CURR_DIR, 'cnvphoto.ini')
@@ -12,7 +13,7 @@ def create_ini(file_ini):
     config = ConfigParser()
     config.add_section('Main')
     config.set('Main', 'work_dir', str(CURR_DIR))
-    config.set('Main', 'max_length_or_width', '800')
+    config.set('Main', 'max_length_or_width', '768')
     with open(file_ini, 'w') as f:
         config.write(f)
 
@@ -27,15 +28,28 @@ def read_ini():
     WORK_DIR = config.get('Main', 'work_dir')
     MAX_LENGTH_OR_WIDTH = int(config.get('Main', 'max_length_or_width'))
 
+def drawProgressBar(n_file, n_files, n_resize):
+    sys.stdout.write(f'\rПросмотрено файлов: {n_file}/{n_files}, изменено файлов: {n_resize}')
+    sys.stdout.flush()
+
 def resize_file(dir):
+    n_files = len(list(Path(dir).glob('*.jpg')))
+    n_resize = 0
+    n_file = 0
     for f in Path(dir).glob('*.jpg'):
-        image = Image.open(f)
-        ratio = max(image.size) / MAX_LENGTH_OR_WIDTH
-        if ratio != 1:
-            size_new = tuple(map(lambda x: int(x / ratio), image.size))
-            print(f'Меняем разрешение файла {f.name}: {image.size} --> {size_new}')
-            image_resize = image.resize(size_new)
-            image_resize.save(f)
+        n_file += 1
+        try:
+            image = Image.open(f)
+            ratio = max(image.size) / MAX_LENGTH_OR_WIDTH
+            if ratio != 1:
+                n_resize += 1
+                size_new = tuple(map(lambda x: int(x / ratio), image.size))
+                image_resize = image.resize(size_new)
+                image_resize.save(f)
+        except:
+            print(f'\nОшибка обработки файла: {f.name}')
+        drawProgressBar(n_file, n_files, n_resize)
+
 
 read_ini()
 resize_file(WORK_DIR)
